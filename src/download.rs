46c62965411fb13a6ac5761a0157cb63746021d2
jbl007 <jbl007@gmail.com>
2025-03-16 14:46:49 +0100

    moved download scope to download crate

--- src/download.rs
+++ src/download.rs
@@ -1,10 +1,91 @@
-use crate::whdload::WhdloadItem;
+use super::remote::create_ftp_stream;
+use super::Credentials;
+use super::whdload::WhdloadItem;
 use anyhow::{Error, Result};
 use std::sync::Mutex;
+use std::thread;
 use suppaftp::{FtpError, FtpStream, Status};
 
 type Queue<'a> = &'a Mutex<Vec<WhdloadItem>>;
 type Requeue = Vec<WhdloadItem>;
+
+// pub const FTP1: &str = "localhost:2121";
+// pub const FTP2: &str = "localhost:2122";
+// pub const FTP3: &str = "localhost:2123";
+pub const FTP1: &str = "ftp.grandis.nu:21";
+pub const FTP2: &str = "ftp2.grandis.nu:21";
+pub const FTP3: &str = "grandis.nu:21";
+
+pub fn download(
+    to_download: Vec<WhdloadItem>,
+    mut ftp2: &mut FtpStream,
+    login: &Credentials,
+) -> Result<Vec<WhdloadItem>, Error> {
+    let num_downloads = to_download.len();
+    let queue = Mutex::new(to_download);
+
+    thread::scope(|scope| {
+        let mut threads = vec![];
+        let mut failed_downloads = vec![];
+
+        let primary = scope.spawn(|| run_downloader(&queue, &mut ftp2, true, "FTP2-1"));
+
+        if num_downloads > 2 {
+            threads.push(scope.spawn(|| {
+                create_ftp_stream(FTP1, &login)
+                    .and_then(|mut ftp1| run_downloader(&queue, &mut ftp1, false, "FTP1-1"))
+            }));
+        };
+
+        if num_downloads > 4 && !login.is_anonymous {
+            threads.push(scope.spawn(|| {
+                create_ftp_stream(FTP3, &login)
+                    .and_then(|mut ftp3| run_downloader(&queue, &mut ftp3, false, "FTP3-1"))
+            }));
+        };
+
+        if num_downloads > 6 && !login.is_anonymous {
+            threads.push(scope.spawn(|| {
+                create_ftp_stream(FTP2, &login)
+                    .and_then(|mut ftp2| run_downloader(&queue, &mut ftp2, false, "FTP2-2"))
+            }));
+        };
+
+        if num_downloads > 8 && !login.is_anonymous {
+            threads.push(scope.spawn(|| {
+                create_ftp_stream(FTP1, &login)
+                    .and_then(|mut ftp1| run_downloader(&queue, &mut ftp1, false, "FTP1-2"))
+            }));
+        };
+
+        if num_downloads > 10 && !login.is_anonymous {
+            threads.push(scope.spawn(|| {
+                create_ftp_stream(FTP3, &login)
+                    .and_then(|mut ftp3| run_downloader(&queue, &mut ftp3, false, "FTP3-2"))
+            }));
+        };
+
+        if let Err(e) = primary.join().unwrap() {
+            eprintln!("Primary downloader finished unexpectly.");
+            eprintln!("{e}");
+            {
+                queue.lock().unwrap().clear();
+            };
+            eprintln!("Waiting for threads to finish.");
+            for t in threads {
+                let _ = t.join().unwrap();
+            }
+            return Err(e);
+        }
+        for t in threads {
+            match t.join().unwrap() {
+                Ok(mut failed) => failed_downloads.append(&mut failed),
+                Err(e) => eprintln!("{e}"),
+            };
+        }
+        Ok(failed_downloads)
+    })
+}
 
 pub fn run_downloader(
     items: Queue,
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,14 @@ use std::env::{args, set_current_dir};
 use std::fs::remove_file;
 use std::path::PathBuf;
 use std::sync::Mutex;
-use std::{thread, vec};
-use download::run_downloader;
+use std::thread;
+use download::*;
 use anyhow::{bail, Result};
 use credentials::Credentials;
 use remote::{create_ftp_stream, find_remote_files};
 use local::{find_local_files, remove_old_dats};
 use whdload::WhdloadItem;
 
-// pub const FTP1: &str = "localhost:2121";
-// pub const FTP2: &str = "localhost:2122";
-// pub const FTP3: &str = "localhost:2123";
-pub const FTP1: &str = "ftp.grandis.nu:21";
-pub const FTP2: &str = "ftp2.grandis.nu:21";
-pub const FTP3: &str = "grandis.nu:21";
 
 fn main() -> Result<()> {
     println!("whdsnc2 version 0.2.0");
@@ -50,73 +44,11 @@ fn main() -> Result<()> {
         println!("Downloading {num_downloads} files.");
     }
 
-    let queue = Mutex::new(to_download);
-    let mut failed_downloads = vec![];
-
-    let dl_scope = thread::scope(|scope| {
-        let mut threads = vec![];
-        let primary = scope.spawn(|| run_downloader(&queue, &mut ftp2, true, "FTP2-1"));
-
-        if num_downloads > 2 {
-            threads.push(scope.spawn(|| {
-                create_ftp_stream(FTP1, &login)
-                    .and_then(|mut ftp1| run_downloader(&queue, &mut ftp1, false, "FTP1-1"))
-            }));
-        };
-
-        if num_downloads > 4 && !login.is_anonymous {
-            threads.push(scope.spawn(|| {
-                create_ftp_stream(FTP3, &login)
-                    .and_then(|mut ftp3| run_downloader(&queue, &mut ftp3, false, "FTP3-1"))
-            }));
-        };
-
-        if num_downloads > 6 && !login.is_anonymous {
-            threads.push(scope.spawn(|| {
-                create_ftp_stream(FTP2, &login)
-                    .and_then(|mut ftp2| run_downloader(&queue, &mut ftp2, false, "FTP2-2"))
-            }));
-        };
-
-        if num_downloads > 8 && !login.is_anonymous {
-            threads.push(scope.spawn(|| {
-                create_ftp_stream(FTP1, &login)
-                    .and_then(|mut ftp1| run_downloader(&queue, &mut ftp1, false, "FTP1-2"))
-            }));
-        };
-
-        if num_downloads > 10 && !login.is_anonymous {
-            threads.push(scope.spawn(|| {
-                create_ftp_stream(FTP3, &login)
-                    .and_then(|mut ftp3| run_downloader(&queue, &mut ftp3, false, "FTP3-2"))
-            }));
-        };
-
-        if let Err(e) = primary.join().unwrap() {
-            eprintln!("Primary downloader finished unexpectly.");
-            eprintln!("{e}");
-            {
-                queue.lock().unwrap().clear();
-            };
-            eprintln!("Waiting for threads to finish.");
-            for t in threads {
-                let _ = t.join().unwrap();
-            }
-            return Err(e);
-        }
-        for t in threads {
-            match t.join().unwrap() {
-                Ok(mut failed) => failed_downloads.append(&mut failed),
-                Err(e) => eprintln!("{e}"),
-            };
-        }
-        Ok(())
-    });
-
-    if dl_scope.is_err() {
-        bail!("Fatal error. Can't continue.")
+    let failed_downloads = match download(to_download, &mut ftp2, &login) {
+        Ok(f) => f,
+        Err(e) => return Err(e)
     };
-
+    
     let num_failed = failed_downloads.len();
 
     let success = if num_failed == 0 {
